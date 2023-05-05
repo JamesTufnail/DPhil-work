e097af223e3a1258e8d3f77e2ae8fffbf190b098
James Tufnail <jamestufnail@outlook.com>
2023-05-05 19:32:46 +0100

    Added 2 new functions: 1 - to print indices (were sweep rates change) 2 - to plot all sweep rates on one plot based on indices

--- XRD_TA_example/main.py
+++ XRD_TA_example/main.py
@@ -5,17 +5,47 @@ from plotting_functions import *
 from mawatari_functions import *
 import glob
 
+# selected_files = mawatari_files[:]
+
+
+
 ### Code snippet to plot multiple mawatari files in loop
 mawatari_files = sorted(
     glob.glob(r"C:\Users\James\OneDrive - Nexus365\DPhil-general\Birmingham Neutron Irradiation\Birmingham "
               r"data\OneDrive_2023-01-30\2023-01-20 Neutron Irradiation of Disk Samples - Lot 1 ("
               r"copy)\Fu21Gdo_1\Irr0\PPMS Data\22 12 07\Mawatari*.csv"))
+#field_moment_plot(mawatari_files)
 
-# selected_files = mawatari_files[:]
-field_moment_plot(mawatari_files)
+indices, data_df = find_indices(mawatari_files[0])
+print(indices)
+enumerated = enumerate(indices)
+print(enumerated)
+multiple_raw_mawatari(data_df, indices)
 
-for file in mawatari_files:
-    indexing_and_slicing(file)
+# for file in mawatari_files:
+#     file_index = find_indices(file)
+#     print(file_index)
+
+    # sliced_data = indexing_and_slicing(file)
+    # print(sliced_data)
+    #multiple_raw_mawatari(data_df)
+    ## TODO: convert the units here before the loop below!
+    #cols = sliced_data.columns.to_numpy()
+    # print(enumerate(cols))
+    # plt.plot(sliced_data[cols[1]], sliced_data[cols[2]])
+    # plt.show()
+
+    # for i in range(len(sliced_data.columns)-2):
+    #     plt.plot(sliced_data.iloc[:, i+1], sliced_data.iloc[:, i+2])
+    # plt.show()
+
+    # for col in cols[1:]:
+    #     plt.plot()
+    #
+    #
+    # for col in cols[1:]:
+    #     plt.plot(sliced_data[col], sliced_data[str(col+1)])
+    #     plt.show()
 
 
 
